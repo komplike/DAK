90d47bc0970e858ba71fca6ec404f9eec53b3f55
komplike <romansan.tn@gmail.com>
2020-03-06 10:41:54 +0100

    Add files via upload

--- c2/u1.m
+++ c2/u1.m
@@ -8,18 +8,12 @@ G = [1 0 0 1 1 1 0; 0 1 0 0 1 1 1; 0 0 1 1 0 1 1];
 f = mod(p*G,2);
 
 %% dekodovanie LB (7,4)
-N = 7;
-k = 4;
-
 F = [1 1 0 1 1 1 0; 1 1 1 0 1 0 1; 1 0 0 1 0 1 1];
 G = [1 0 0 0 1 0 1; 0 1 0 0 0 1 1; 0 0 1 0 1 1 1; 0 0 0 1 1 1 0];
 
 H=[];
-r=N-k;
-[fin,x]=size(G);
-for n=1:fin
-    H = [H; G(n,k+1) G(n,k+2) G(n,k+3)];
-end
-
-
+[k,N]=size(G);
+Ht = [G(:,k+1:N);eye(N-k)]
+disp('syndrom');
+mod(F*Ht,2)
 
